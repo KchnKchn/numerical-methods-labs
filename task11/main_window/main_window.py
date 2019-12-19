6429e5cbcf8d1f9092c117047985295dd0068f40
KchnKchn <khvatov.alexander@gmail.com>
2019-12-19 13:00:35 +0300

    task11 updated

--- task11/main_window/main_window.py
+++ task11/main_window/main_window.py
@@ -148,7 +148,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Численные методы. Лабораторная работа 1. Решение системы ДУ методом РК 4 порядка"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Численные методы. Задача 11. Решение системы ДУ методом РК 4 порядка"))
         self.main_description.setHtml(_translate("MainWindow",""" <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
 <html><head><meta name="qrichtext" content="1" /><style type="text/css">
 p, li { white-space: pre-wrap; }
